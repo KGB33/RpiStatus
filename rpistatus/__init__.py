2690a25b40f9e034140855b6d8157219c06db8aa
KGB33 <github@bassingthwaite.org>
2020-11-25 10:11:10 -0700

    registed Bps

--- rpistatus/__init__.py
+++ rpistatus/__init__.py
@@ -13,8 +13,14 @@ def create_app(config=os.environ["APP_CONFIG"]):
 
     # ----- Register Blueprints -----
     from rpistatus.main import bp as main_bp
+    from rpistatus.hardware import bp as hardware_bp
+    from rpistatus.network import bp as network_bp
+    from rpistatus.docker import bp as docker_bp
 
     app.register_blueprint(main_bp, url_prefix="")
+    app.register_blueprint(hardware_bp, url_prefix="/hardware")
+    app.register_blueprint(network_bp, url_prefix="/network")
+    app.register_blueprint(docker_bp, url_prefix="/docker")
 
     # ----- Register Errors -----
 
