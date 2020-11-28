7fecb8e86e25989fcc34ead46d1cf4638db875e7
KGB33 <github@bassingthwaite.org>
2020-11-28 11:23:49 -0700

    added '/ps' endpoint & tests

--- rpistatus/main/routes.py
+++ rpistatus/main/routes.py
@@ -1,3 +1,5 @@
+import psutil
+
 from rpistatus.main import bp
 
 
@@ -12,3 +14,9 @@ def ps():
     """
     Returns currently running processes
     """
+    return {
+        p.pid: p.info
+        for p in psutil.process_iter(
+            ["name", "status", "username", "memory_percent", "cpu_percent"]
+        )
+    }
--- tests/test_main_routes.py
+++ tests/test_main_routes.py
@@ -3,7 +3,8 @@ def test_index(test_client):
     Given a running RPiStatus app,
     Assert that the `/` endpoint returns a 200 status code
     """
-    assert False
+    response = test_client.get("/")
+    assert response.status_code == 200
 
 
 def test_ps(test_client):
@@ -11,4 +12,14 @@ def test_ps(test_client):
     When the `/ps` route is called,
     Assert that a Json object with the expected headers is returned.
     """
-    assert False
+    response = test_client.get("/ps")
+    assert response.status_code == 200
+
+    assert list(response.json.keys())[0].isdigit()
+    assert set(list(response.json.values())[0].keys()) == {
+        "cpu_percent",
+        "memory_percent",
+        "name",
+        "status",
+        "username",
+    }
